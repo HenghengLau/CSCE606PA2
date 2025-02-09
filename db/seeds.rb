3eee1d24dbe1225bf82fd1eb8180a8fe0cba620c
Zengqi Liu <cr400bfc5145@gmail.com>
2025-02-09 16:40:23 -0600

    More movies added in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Weathering With You', :rating => 'PG-13',
+    :release_date => '19-Jul-2019'},
+  {:title => 'Your Name', :rating => 'PG',
+    :release_date => '03-Jul-2016'},
+  {:title => 'The Classic', :rating => 'PG-13',
+    :release_date => '15-Dec-2006' }
 ]
 
 more_movies.each do |movie|
